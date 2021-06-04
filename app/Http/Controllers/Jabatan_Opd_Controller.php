8f6a975c22a3bf6aeabcd446efeb81396552ff94
ibalpalaloi <ibalpalaloi@gmil.com>
2021-06-04 15:46:04 +0800

    perbaikan

---
+++ app/Http/Controllers/Jabatan_Opd_Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Jabatan_Opd_Controller extends Controller
+{
+    //
+}
--- resources/views/layouts/admin_LTE_Layout.blade.php
+++ resources/views/layouts/admin_LTE_Layout.blade.php
@@ -269,6 +269,25 @@
               </li>
             </ul>
           </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Jabatan
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="/daftar_jabatan" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Daftar jabatan</p>
+                </a>
+              </li>
+            
+            </ul>
+          </li>
+          
          
         </ul>
       </nav>
